b3b0f0da32a571bfbdce58dc5e29565961070ac7
Federico Kauffman <federico@wyeworks.com>
2017-07-20 09:10:55 -0300

    Add button and script

---
+++ example-1-deploy-and-rollback/src/assets/scripts/index.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('button');
+
+    button.addEventListener('click', function(){
+        console.log('Button was clicked!');
+    })
+});
